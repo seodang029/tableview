692f21f9f3af3a7e9c80f47c4f804cd3cdad59e2
csd5766 <csd5766@naver.com>
2018-05-03 15:43:12 +0900

    테이블

--- tableview/ViewController.swift
+++ tableview/ViewController.swift
@@ -8,18 +8,48 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate{
+   
+    var animals = ["cat", "Tiger", "lion","Monkey", "Cow"]
+    var country = ["Korea", "china", "Africa", "Japan", "Aue"]
+    
+    @IBOutlet weak var mytable: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //dategate와 ViewController의 연결
+        
+        mytable.delegate = self
+        mytable.dataSource = self
+    }
+ 
+    //row의 수
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return animals.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // cell의 재사용
+        let identifier = "RE"
+        // cell을 하나 메모리 할당하고 재사용을 위해 저장("RE")
+        let cell = mytable.dequeueReusableCell(withIdentifier: identifier, for: indexPath)
+        
+        // 배열에 있는 데이터를 cell에 넣기
+        
+        cell.textLabel?.text = animals[indexPath.row]
+        cell.detailTextLabel?.text = country[indexPath.row]
+        
+        
+        //이미지 넣기
+        
+        let myImage = UIImage(named: "2.png")
+        cell.imageView?.image = myImage
+        
+        return cell
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
+    
 }
 
